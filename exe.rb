6bdf76047e9559283edd51c68527b9dec548c11f
soma <somofgit@gmail.com>
2023-12-23 12:14:41 +0900

    最新

--- exe.rb
+++ exe.rb
@@ -3,20 +3,28 @@ require 'active_record'
 require 'csv'
 require 'pry'
 
-# 共通ファイル
+# # 共通ファイル
 require_relative 'common'
-# ここでcsv系を追加しておきたい
+# # ここでcsv系を追加しておきたい
 
-# モデル読み込む
+# # モデル読み込む
 require_relative 'models/todo'
+require_relative 'models/user'
 
-# マイグレーションのON
-Migrate.start
+# # マイグレーションのON
+# Migrate.start
 
-# サンプルデータ読み込み
-Todo.setup
+# # サンプルデータ読み込み
+# Todo.setup
 
-Todo.import('import.csv')
+# Todo.import('import.csv')
 
-# マイグレーションOFF
-Migrate.stop
+# # マイグレーションOFF
+# Migrate.stop
+
+
+
+# csv/import内の全パスを取得する
+Dir.children('csv/samples')
+# ファイル名からモデルを絞り込む　なければ異物混入警報発令
+p Dir.children('csv/samples').map{|name| name[/.*(?=\.)/].classify.constantize}
