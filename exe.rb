b407c7fba7976fd32df15618af32a23ec5fdaea6
soma <somofgit@gmail.com>
2024-01-04 09:25:49 +0900

    最新

--- exe.rb
+++ exe.rb
@@ -2,6 +2,8 @@
 require 'active_record'
 require 'csv'
 require 'pry'
+require 'webrick'
+require 'erb'
 
 # # 共通ファイル
 require_relative 'common'
@@ -14,7 +16,12 @@ require_relative 'models/user'
 # # マイグレーションのON
 # Migrate.start
 
+@users = User.all
+
+# webサーバー作成
+# srv = WEBrick::HTTPServer.new({ :DocumentRoot => './index.html', :BindAddress => '127.0.0.1', :Port => 20080})
+# trap("INT"){ srv.shutdown }
+# srv.start
+
 # # マイグレーションOFF
 # Migrate.stop
-
-User.import('user.csv')
