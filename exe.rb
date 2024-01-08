598f2f46f600f7368ed9a0a1d9675fbd170c8421
soma <somofgit@gmail.com>
2024-01-08 23:22:33 +0900

    最新

--- exe.rb
+++ exe.rb
@@ -10,7 +10,7 @@ require_relative 'common'
 require_relative 'models/todo'
 require_relative 'models/user'
 
-# # マイグレーションのON
+# マイグレーションのON
 # Migrate.start
 
 if User.all.blank?
@@ -23,11 +23,37 @@ if User.all.blank?
 end
 
 # webサーバー作成
-srv = WEBrick::HTTPServer.new({ :DocumentRoot => './views/index.html', :BindAddress => '127.0.0.1', :Port => 20080})
-srv.mount_proc('/view') do |_req, res|
+class UserERBHandler < WEBrick::HTTPServlet::AbstractServlet
+  def initialize(server, name)
+    super(server, name)
+    @script_filename = name
+  end
+
+  def do_GET(req, res)
+    data = File.open(@script_filename, &:read)
+    res.body = ERB.new(data, trim_mode: '-').result
+    res['content-type'] ||= WEBrick::HTTPUtils::mime_type(@script_filename, @config[:MimeTypes])
+  end
+end
+WEBrick::HTTPServlet::FileHandler.add_handler("erb", UserERBHandler)
+
+srv = WEBrick::HTTPServer.new({ DocumentRoot: 'public', DirectoryIndex: ['user.html.erb'], :BindAddress => '127.0.0.1', :Port => 20080, :ServerName => 'マイサーバー' })
+srv.mount_proc('/new') do |req, res|
   res.status = 200
   res.content_type = 'text/html'
-  res.body = ERB.new( File.read('views/user.html.erb') , trim_mode: '-').result
+  res.body = ERB.new( File.read('public/user_new.html.erb') , trim_mode: '-').result
+
+  if req.query['name'].present?
+    binding.pry
+    # マルチバイト文字（あ）を送信すると、エラーになる
+
+    # User.create(name: req.query['name'])
+    # 
+    # リクエストメソッドをpostからgetにする
+    # res.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, "/")
+    res.set_redirect(WEBrick::HTTPStatus::MovedPermanently, '/')
+  end
 end
+
 trap("INT"){ srv.shutdown }
 srv.start
