55486a57ca25ca17c234dac7273dee32432e8c6c
soma <somofgit@gmail.com>
2024-01-13 15:03:26 +0900

    最新

--- exe.rb
+++ exe.rb
@@ -11,6 +11,7 @@ require_relative 'models/todo'
 require_relative 'models/user'
 
 # マイグレーションのON
+# Migrate.stop
 # Migrate.start
 
 if User.all.blank?
@@ -38,20 +39,34 @@ end
 WEBrick::HTTPServlet::FileHandler.add_handler("erb", UserERBHandler)
 
 srv = WEBrick::HTTPServer.new({ DocumentRoot: 'public', DirectoryIndex: ['user.html.erb'], :BindAddress => '127.0.0.1', :Port => 20080, :ServerName => 'マイサーバー' })
+
+# CGI.unescape https://github.com/ruby/cgi/blob/929e6264b519a6b6d1487d0ba5cea48579dc3c0d/lib/cgi/util.rb#L27C3-L35C6
+def unescape(string)
+  str = string.tr('+', ' ')
+  str = str.b
+  str.gsub!(/((?:%[0-9a-fA-F]{2})+)/) do |m|
+    [m.delete('%')].pack('H*')
+  end
+  str.force_encoding(Encoding::UTF_8)
+  str.valid_encoding? ? str : str.force_encoding(string.Encoding::UTF_8)
+end
+
 srv.mount_proc('/new') do |req, res|
-  res.status = 200
-  res.content_type = 'text/html'
-  res.body = ERB.new( File.read('public/user_new.html.erb') , trim_mode: '-').result
-
   if req.query['name'].present?
-    binding.pry
-    # マルチバイト文字（あ）を送信すると、エラーになる
-
-    # User.create(name: req.query['name'])
-    # 
-    # リクエストメソッドをpostからgetにする
-    # res.set_redirect(WEBrick::HTTPStatus::TemporaryRedirect, "/")
+    User.create(name: unescape(req.query['name']))
     res.set_redirect(WEBrick::HTTPStatus::MovedPermanently, '/')
+  end
+  res.body = ERB.new( File.read('public/user_new.html.erb') , trim_mode: '-').result
+end
+
+User.all.each do |user| #=> TODO 新規作成したユーザーを編集しようとすると404エラーになる。作成したものは、マウントしないからだ。
+  srv.mount_proc("/#{user.id}/edit") do |req, res|
+    @view_item = user
+    if req.query['name'].present? && user.name != (update_value = unescape(req.query['name'])) #=> req.query['name'].encode("UTF-8")でエラーになる理由
+      User.update(user.id, name: update_value)
+      res.set_redirect(WEBrick::HTTPStatus::MovedPermanently, '/')
+    end
+    res.body = ERB.new( File.read('public/user_edit.html.erb') , trim_mode: '-').result
   end
 end
 
