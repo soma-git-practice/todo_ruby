ef1c885058e26b8d9e9bf13e824ec8e779da69fb
soma <somofgit@gmail.com>
2024-02-08 22:23:35 +0900

    最新

--- calender.rb
+++ calender.rb
@@ -6,11 +6,12 @@ begining_of_the_month = Date.new(*origin_ym, +1)
 ending_of_the_month   = Date.new(*origin_ym, -1)
 
 during = begining_of_the_month..ending_of_the_month
-# during.find_all do |day|
-#   puts day
-# end
+during.find_all do |day|
+  puts day
+end
 
-#  週は七日
+
+
 
 # | sund | mond | tues | wedn | thur | frid | satu |
 # |      |      |      |      |  01  |  02  |  03  |
@@ -19,7 +20,17 @@ during = begining_of_the_month..ending_of_the_month
 # |  18  |  19  |  20  |  21  |  22  |  23  |  24  |
 # |  25  |  26  |  27  |  28  |  29  |      |      |
 
-puts "| sund | mond | tues | wedn | thur | frid | satu |\n|      |      |      |      |  01  |  02  |  03  |\n|  04  |  05  |  06  |  07  |  08  |  09  |  10  |\n|  11  |  12  |  13  |  14  |  15  |  16  |  17  |\n|  18  |  19  |  20  |  21  |  22  |  23  |  24  |\n|  25  |  26  |  27  |  28  |  29  |      |      |"
+w = ['SUND', 'MOND', 'TUES', 'WEDE', 'THUR', 'FRID', 'SATU', '00', '00', '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14']
 
+# 最大文字数を計算
+max_value = w.map(&:length).max
 
-# 7回目が#になるコード
+# 中央寄せ
+result = w.map { |item| item.center(max_value + 2) }
+
+# 7分割
+result = result.each_slice(7).map do |item|
+  "|" + item * "|" + "|"
+end
+
+puts result
