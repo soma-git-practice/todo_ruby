5ce78f2ba05fa89fc9feaaabc6e56f382d4d703a
soma <somofgit@gmail.com>
2024-02-08 23:14:50 +0900

    汚いがカレンダーにはなった

--- calender.rb
+++ calender.rb
@@ -1,17 +1,4 @@
 require 'date'
-origin = Date.today
-origin_ym = origin.year, origin.month
-
-begining_of_the_month = Date.new(*origin_ym, +1)
-ending_of_the_month   = Date.new(*origin_ym, -1)
-
-during = begining_of_the_month..ending_of_the_month
-during.find_all do |day|
-  puts day
-end
-
-
-
 
 # | sund | mond | tues | wedn | thur | frid | satu |
 # |      |      |      |      |  01  |  02  |  03  |
@@ -20,7 +7,17 @@ end
 # |  18  |  19  |  20  |  21  |  22  |  23  |  24  |
 # |  25  |  26  |  27  |  28  |  29  |      |      |
 
-w = ['SUND', 'MOND', 'TUES', 'WEDE', 'THUR', 'FRID', 'SATU', '00', '00', '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14']
+origin = Date.today
+# origin_ym = origin.year, origin.month
+origin_ym = 2024, 2
+
+begining_of_the_month = Date.new(*origin_ym, +1)
+ending_of_the_month   = Date.new(*origin_ym, -1)
+
+during = begining_of_the_month..ending_of_the_month
+
+w = ['SUND', 'MOND', 'TUES', 'WEDE', 'THUR', 'FRID', 'SATU']
+w += (0...during.first.wday).map{ '  ' } + during.map { |day| "%02d" % day.day } + Array.new(6 - during.last.wday, '  ')
 
 # 最大文字数を計算
 max_value = w.map(&:length).max
